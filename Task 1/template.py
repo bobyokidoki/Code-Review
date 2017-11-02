bc9214a883d00af46f2f4e99cf0809dd8aaefed6
bobyokidoki <bobyokidoki92@gmail.com>
2017-11-02 16:49:41 +0000

    Test ... test ...

--- Task 1/template.py
+++ Task 1/template.py
@@ -8,7 +8,7 @@ def find_stars(ccd_image):
 	Given a `ccd_image`, return an astropy table containing columns called x, y, flux
 	"""
 	
-	catalogue = 0 # test
+	catalogue = 0 # test line 
 
 
 	return catalogue
